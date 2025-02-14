6e326a36ebf65a537b2bfd915ebdad68f9030759
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-02-14 20:50:13 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/apitareas/ApiTareasApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apitareas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTareasApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiTareasApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/apitareas/ApiTareasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apitareas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiTareasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
